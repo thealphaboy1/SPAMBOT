b06c9198f88985c6ff01c3bc3c3330ce554a566f
OXYEGN <166620351+BWANDARLOK@users.noreply.github.com>
2024-05-01 00:04:56 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -8,11 +8,11 @@ START_BUTTON = [
         Button.inline("★𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦★", data="help_back")
     ],
     [
-        Button.url("★𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥★", "https://t.me/OXEGN"),
-        Button.url("★𝗦𝗨𝗣𝗣𝗢𝗥𝗧★", "https://t.me/+XpL0qhdF7TQzNDVl")
+        Button.url("★𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥★", "https://t.me/chalcogen645"),
+        Button.url("★𝗦𝗨𝗣𝗣𝗢𝗥𝗧★", "https://t.me/bestfriesalways")
     ],
     [
-        Button.url("★𝗖𝗢𝗗𝗘𝗥★", "https://t.me/PRADHAN474")
+        Button.url("★𝗖𝗢𝗗𝗘𝗥★", "https://t.me/OXEGN")
     ]
 ]
 
@@ -35,7 +35,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/b11e7d86e4622a3b3e54e.jpg",
+            "https://telegra.ph/file/ea4802bf0511a0510e825.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )

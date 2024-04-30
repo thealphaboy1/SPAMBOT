386b373f04ae997d7ea26657192df2dc9e2957de
OXYEGN <166620351+BWANDARLOK@users.noreply.github.com>
2024-05-01 00:09:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["OXYBOT.modules." + plugin_name] = load
-    print("𝗢𝗫𝗬𝗚𝗘𝗡 𝗛𝗔𝗦 𝗜𝗠𝗣𝗢𝗥𝗧𝗘𝗗 " + plugin_name)
+    print("𝗖𝗛𝗔𝗟𝗖𝗢𝗚𝗘𝗡 𝗛𝗔𝗦 𝗜𝗠𝗣𝗢𝗥𝗧𝗘𝗗 " + plugin_name)
 
 
 files = glob.glob("OXYBOT/modules/*.py")
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\𝗢𝗫𝗬𝗚𝗘𝗡 𝗦𝗣𝗔𝗠 𝗕𝗢𝗧 𝗗𝗘𝗣𝗟𝗢𝗬𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬⚡\nMy Master ---> 🦋⃟ ᴠͥɪͣᴘͫ 🇴 🇽 𝐘 𝐆 𝐄 𝐍⃝⃝⃪⃕🥀🦋͜͡•─⃛͢ @PRADHAN474")
+print("\𝗖𝗛𝗔𝗟𝗖𝗢𝗚𝗘𝗡 𝗦𝗣𝗔𝗠 𝗕𝗢𝗧 𝗗𝗘𝗣𝗟𝗢𝗬𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬⚡\nMy Master --->↬ᬃ ⃪ ⷬ ⷡ ⷯ⌯𝐏 𝐁 𝐗 𝟏 🜲 💸 𝐂𝐇𝐀𝐋𝐂𝐎𝐆𝐄𝐍")
 
 
 async def main():
